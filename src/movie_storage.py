13cab83eead6154959dd3e561a7ead8b8d7831c1
johannes <johannes.lewen@lewen.eu>
2025-05-07 20:58:32 +0200

    add docstrings

--- src/movie_storage.py
+++ src/movie_storage.py
@@ -1,6 +1,6 @@
-"""Decouple manger from database interaction
-in terms of I/O processes. This way the DB could be swapped
-without touching the logic of interaction, i.e. MovieManager."""
+"""Handle database interactions in terms of I/O processes.
+This way the DB could be swapped without touching
+the logic of interaction, i.e. MovieManager."""
 
 import json
 
@@ -17,6 +17,8 @@ class MovieStorage:
         return movie_data
 
     def _save_movies(self, movies: dict):
+        """Private method for saving the data to the json file.
+        Should only be called through the appropriate methods."""
         with open(self.db_path, 'w', encoding='utf-8') as json_file:
             json.dump(movies, json_file)
 
@@ -27,11 +29,15 @@ class MovieStorage:
         self._save_movies(self.movies)
 
     def update_movie(self, title: str, movie_info: dict):
+        """Update movie with new information.
+        Technically the same as add_movie for my json setup, but
+        will behave differently when possibly changing dbs -> see README.md"""
         self.movies = self.get_movies()
         self.movies[title] = movie_info
         self._save_movies(self.movies)
 
     def delete_movie(self, title: str):
+        """Delete movie from database."""
         self.movies = self.get_movies()
         del self.movies[title]
         self._save_movies(self.movies)
